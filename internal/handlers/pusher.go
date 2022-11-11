61524573cd668d82d55faadcdbd0e67945816e15
Santosh Kumar <sntshkmr60@gmail.com>
2022-11-11 20:40:01 +0530

    Connect to pusher server upon start

---
+++ internal/handlers/pusher.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/pusher/pusher-http-go"
+)
+
+func (repo *DBRepo) PusherAuth(w http.ResponseWriter, r *http.Request) {
+	userID := repo.App.Session.GetInt(r.Context(), "userID")
+	u, _ := repo.DB.GetUserById(userID)
+
+	params, _ := io.ReadAll(r.Body)
+	presenceData := pusher.MemberData{
+		UserID: strconv.Itoa(userID),
+		UserInfo: map[string]string{
+			"name": u.FirstName,
+			"id":   strconv.Itoa(userID),
+		},
+	}
+
+	response, err := app.WsClient.AuthenticatePresenceChannel(params, presenceData)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response)
+}
