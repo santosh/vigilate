3396a7739b5bbd50cb4b8a332bad032ef76c2e66
Santosh Kumar <sntshkmr60@gmail.com>
2022-11-16 13:08:15 +0530

    Check HTTP service status

--- internal/handlers/perform-checks.go
+++ internal/handlers/perform-checks.go
@@ -2,12 +2,21 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
+	"github.com/tsawler/vigilate/internal/models"
+)
+
+const (
+	HTTP           = 1
+	HTTPS          = 2
+	SSLCertificate = 3
 )
 
 type jsonResp struct {
@@ -34,20 +43,53 @@ func (repo *DBRepo) TestCheck(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	log.Println("Service name is", hs.Service.ServiceName)
-
 	// get host?
+	h, err := repo.DB.GetHostByID(hs.HostID)
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	// test the service
+	msg, newStatus := repo.testServiceForHost(h, hs)
+	log.Println(msg, newStatus)
 
 	// create json
-
-	// send json to client
 	resp := jsonResp{
 		OK:      true,
 		Message: "test message",
 	}
+
+	// send json to client
 	out, _ := json.MarshalIndent(resp, "", "  ")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
+}
+
+func (repo *DBRepo) testServiceForHost(h models.Host, hs models.HostService) (string, string) {
+	var msg, newStatus string
+
+	switch hs.ServiceID {
+	case HTTP:
+		msg, newStatus = testHTTPForHost(h.URL)
+	}
+
+	return msg, newStatus
+}
+
+func testHTTPForHost(url string) (string, string) {
+	url = strings.TrimSuffix(url, "/")
+	url = strings.Replace(url, "https://", "http://", -1)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Sprintf("%s - %s", url, "error connecting"), "problem"
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Sprintf("%s - %s", url, resp.Status), "problem"
+	}
+
+	return fmt.Sprintf("%s - %d", url, resp.StatusCode), "healthy"
 }
