306269ceb78cf570b888d1fbc9a95def5216cac3
Santosh Kumar <sntshkmr60@gmail.com>
2022-11-17 11:08:20 +0530

    Create monitoring payload and push it go WS

--- cmd/web/setup-app.go
+++ cmd/web/setup-app.go
@@ -139,6 +139,8 @@ func setupApp() (*string, error) {
 	log.Println("Secure", *pusherSecure)
 
 	app.WsClient = wsClient
+	monitorMap := make(map[int]cron.EntryID)
+	app.MonitorMap = monitorMap
 	localZone, _ := time.LoadLocation("Local")
 	scheduler := cron.New(cron.WithLocation(localZone), cron.WithChain(
 		cron.DelayIfStillRunning(cron.DefaultLogger),
--- cmd/web/start-monitoring.go
+++ cmd/web/start-monitoring.go
@@ -1,6 +1,11 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
 
 type job struct {
 	HostServiceID int
@@ -35,9 +40,53 @@ func startMonitoring() {
 			log.Println("Service to monitor on", hs.HostName, "is", hs.Service.ServiceName)
 
 			// get the schedule unit and number
+			var sch string
+			if hs.ScheduleUnit == "d" {
+				sch = fmt.Sprintf("@every %d%s", hs.ScheduleNumber*24, "h")
+			} else {
+				sch = fmt.Sprintf("@every %d%s", hs.ScheduleNumber, hs.ScheduleUnit)
+			}
+
 			// create a job
+			var j job
+			j.HostServiceID = hs.ID
+			scheduleID, err := app.Scheduler.AddJob(sch, j)
+			if err != nil {
+				log.Println(err)
+			}
+
 			// save the id of the job so we can start/stop it
+			app.MonitorMap[hs.ID] = scheduleID
+
 			// broadcast over websockets the fact that the service is scheduled
+			payload := make(map[string]string)
+			payload["message"] = "scheduling"
+			payload["host_service_id"] = strconv.Itoa(hs.ID)
+			yearOne := time.Date(0001, 11, 17, 20, 34, 58, 65138737, time.UTC)
+			if app.Scheduler.Entry(app.MonitorMap[hs.ID]).Next.After(yearOne) {
+				payload["next_run"] = app.Scheduler.Entry(app.MonitorMap[hs.ID]).Next.Format("2006-01-02 3:04:05 PM")
+			} else {
+				payload["next_run"] = "Pending..."
+			}
+
+			payload["host"] = hs.HostName
+			payload["service"] = hs.Service.ServiceName
+			if hs.LastCheck.After(yearOne) {
+				payload["last_run"] = hs.LastCheck.Format("2006-01-02 3:04:05 PM")
+			} else {
+				payload["last_run"] = "Pending..."
+			}
+			payload["schedule"] = fmt.Sprintf("@every %d%s", hs.ScheduleNumber, hs.ScheduleUnit)
+
+			err = app.WsClient.Trigger("public-channel", "next-run-event", payload)
+			if err != nil {
+				log.Println(err)
+			}
+
+			err = app.WsClient.Trigger("public-channel", "schedule-changed-event", payload)
+			if err != nil {
+				log.Println(err)
+			}
 		}
 	}
 }
